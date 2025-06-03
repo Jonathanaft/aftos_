1a5ab65b7d8d9c1cb89b90b3e96db52df42cbd0d
Jonathanaft <152176072+Jonathanaft@users.noreply.github.com>
2025-06-03 19:24:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,20 @@
-const CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'reaftos-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
   '/wallpaper.png',
-  // add other assets you want cached
+  '/index.html',
+  // Add other assets you want to cache for offline use
 ];
 
 self.addEventListener('install', event => {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Opened reaftos cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Activate event - clean up old caches if needed
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => 
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
-});
-
-// Fetch event - serve cached content when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -42,36 +23,7 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Because we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the request.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          response => {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetch(event.request);
       })
-  );
-});
-.request).then(response => response || fetch(event.request))
   );
 });
